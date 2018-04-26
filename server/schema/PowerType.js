d5f57acec06be1646361adb49b884c3335bd0c66
Alex <alexrminnick@gmail.com>
2018-04-26 11:43:58 -0700

    change

--- server/schema/PowerType.js
+++ server/schema/PowerType.js
@@ -1,10 +1,10 @@
 const graphql = require('graphql')
-const { GraphQLString, GraphQLObjectType } = graphql
+const { GraphQLString, GraphQLObjectType, GraphQLInt } = graphql
 
 const PowerType = new GraphQLObjectType({
   name: 'Power',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLInt },
     power: { type: GraphQLString },
     description: { type: GraphQLString }
   })
